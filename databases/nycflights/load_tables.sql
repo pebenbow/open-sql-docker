e5d48aa22f9bd54baba33fcb70a0887a6c75228a
Pete Benbow <pebenbow@davidson.edu>
2026-03-24 10:24:28 -0400

    Updated nycflights load script to remove data subdirectory

--- databases/nycflights/load_tables.sql
+++ databases/nycflights/load_tables.sql
@@ -1,5 +1,5 @@
-COPY public.airlines FROM '/data/airlines.csv' DELIMITER ',' CSV HEADER;
-COPY public.airports FROM '/data/airports.csv' DELIMITER ',' CSV HEADER;
-COPY public.planes   FROM '/data/planes.csv'   DELIMITER ',' CSV HEADER;
-COPY public.weather  FROM '/data/weather.csv'  DELIMITER ',' CSV HEADER;
-COPY public.flights  FROM '/data/flights.csv'  DELIMITER ',' CSV HEADER;
+COPY public.airlines FROM 'airlines.csv' DELIMITER ',' CSV HEADER;
+COPY public.airports FROM 'airports.csv' DELIMITER ',' CSV HEADER;
+COPY public.planes   FROM 'planes.csv'   DELIMITER ',' CSV HEADER;
+COPY public.weather  FROM 'weather.csv'  DELIMITER ',' CSV HEADER;
+COPY public.flights  FROM 'flights.csv'  DELIMITER ',' CSV HEADER;
