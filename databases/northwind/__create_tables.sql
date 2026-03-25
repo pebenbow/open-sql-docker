82161e36529c290a5724ba8964c38144642b410c
Taehee Kim <106698025+keehtae@users.noreply.github.com>
2026-03-25 15:44:15 -0400

    Updating Column Order

--- databases/northwind/__create_tables.sql
+++ databases/northwind/__create_tables.sql
@@ -2,26 +2,27 @@ CREATE SCHEMA IF NOT EXISTS public;
 
 -- region
 CREATE TABLE public.region (
-    region_description VARCHAR(60),
-    region_id INTEGER PRIMARY KEY
+    region_id INTEGER PRIMARY KEY,
+    region_description VARCHAR(60)
 );
 
 -- categories
 CREATE TABLE public.categories (
+    category_id INTEGER PRIMARY KEY,
     category_name VARCHAR(15),
     description TEXT,
-    picture BYTEA,
-    category_id INTEGER PRIMARY KEY
+    picture BYTEA
 );
 
 -- customer_demographics
 CREATE TABLE public.customer_demographics (
-    customer_desc TEXT,
-    customer_type_id VARCHAR(5) PRIMARY KEY
+    customer_type_id VARCHAR(5) PRIMARY KEY,
+    customer_desc TEXT
 );
 
 -- customers
 CREATE TABLE public.customers (
+    customer_id VARCHAR(5) PRIMARY KEY,
     company_name VARCHAR(40),
     contact_name VARCHAR(30),
     contact_title VARCHAR(30),
@@ -31,19 +32,19 @@ CREATE TABLE public.customers (
     postal_code VARCHAR(10),
     country VARCHAR(15),
     phone VARCHAR(24),
-    fax VARCHAR(24),
-    customer_id VARCHAR(5) PRIMARY KEY
+    fax VARCHAR(24)
 );
 
 -- shippers
 CREATE TABLE public.shippers (
+    shipper_id INTEGER PRIMARY KEY,
     company_name VARCHAR(40),
-    phone VARCHAR(24),
-    shipper_id INTEGER PRIMARY KEY
+    phone VARCHAR(24)
 );
 
 -- suppliers
 CREATE TABLE public.suppliers (
+    supplier_id INTEGER PRIMARY KEY,
     company_name VARCHAR(40),
     contact_name VARCHAR(30),
     contact_title VARCHAR(30),
@@ -54,20 +55,20 @@ CREATE TABLE public.suppliers (
     country VARCHAR(15),
     phone VARCHAR(24),
     fax VARCHAR(24),
-    homepage TEXT,
-    supplier_id INTEGER PRIMARY KEY
+    homepage TEXT
 );
 
 -- us_states
 CREATE TABLE public.us_states (
+    state_id INTEGER PRIMARY KEY,
     state_name VARCHAR(100),
     state_abbr VARCHAR(2),
-    state_region VARCHAR(50),
-    state_id INTEGER PRIMARY KEY
+    state_region VARCHAR(50)
 );
 
 -- employees
 CREATE TABLE public.employees (
+    employee_id INTEGER PRIMARY KEY,
     last_name VARCHAR(20),
     first_name VARCHAR(10),
     title VARCHAR(30),
@@ -85,22 +86,22 @@ CREATE TABLE public.employees (
     notes TEXT,
     reports_to INTEGER,
     photo_path VARCHAR(255),
-    employee_id INTEGER PRIMARY KEY,
     CONSTRAINT fk_employees_reports_to
         FOREIGN KEY (reports_to) REFERENCES public.employees (employee_id)
 );
 
 -- territories
 CREATE TABLE public.territories (
+    territory_id VARCHAR(20) PRIMARY KEY,
     territory_description VARCHAR(60),
     region_id INTEGER,
-    territory_id VARCHAR(20) PRIMARY KEY,
     CONSTRAINT fk_territories_region_id
         FOREIGN KEY (region_id) REFERENCES public.region (region_id)
 );
 
 -- products
 CREATE TABLE public.products (
+    product_id INTEGER PRIMARY KEY,
     product_name VARCHAR(40),
     supplier_id INTEGER,
     category_id INTEGER,
@@ -109,8 +110,7 @@ CREATE TABLE public.products (
     units_in_stock INTEGER,
     units_on_order INTEGER,
     reorder_level INTEGER,
-    discontinued INTEGER,
-    product_id INTEGER PRIMARY KEY,
+    discontinued INTEGER
     CONSTRAINT fk_products_supplier_id
         FOREIGN KEY (supplier_id) REFERENCES public.suppliers (supplier_id),
     CONSTRAINT fk_products_category_id
@@ -119,9 +119,9 @@ CREATE TABLE public.products (
 
 -- customer_customer_demo
 CREATE TABLE public.customer_customer_demo (
-    customer_id VARCHAR(5),
-    customer_type_id VARCHAR(5),
     PRIMARY KEY (customer_id, customer_type_id),
+    customer_id VARCHAR(5),
+    customer_type_id VARCHAR(5)
     CONSTRAINT fk_customer_customer_demo_customer_id
         FOREIGN KEY (customer_id) REFERENCES public.customers (customer_id),
     CONSTRAINT fk_customer_customer_demo_customer_type_id
@@ -130,9 +130,9 @@ CREATE TABLE public.customer_customer_demo (
 
 -- employee_territories
 CREATE TABLE public.employee_territories (
+    PRIMARY KEY (employee_id, territory_id),
     employee_id INTEGER,
     territory_id VARCHAR(20),
-    PRIMARY KEY (employee_id, territory_id),
     CONSTRAINT fk_employee_territories_employee_id
         FOREIGN KEY (employee_id) REFERENCES public.employees (employee_id),
     CONSTRAINT fk_employee_territories_territory_id
@@ -141,6 +141,7 @@ CREATE TABLE public.employee_territories (
 
 -- orders
 CREATE TABLE public.orders (
+    order_id INTEGER PRIMARY KEY,
     customer_id VARCHAR(5),
     employee_id INTEGER,
     order_date DATE,
@@ -154,7 +155,6 @@ CREATE TABLE public.orders (
     ship_region VARCHAR(15),
     ship_postal_code VARCHAR(10),
     ship_country VARCHAR(15),
-    order_id INTEGER PRIMARY KEY,
     CONSTRAINT fk_orders_customer_id
         FOREIGN KEY (customer_id) REFERENCES public.customers (customer_id),
     CONSTRAINT fk_orders_employee_id
@@ -165,12 +165,12 @@ CREATE TABLE public.orders (
 
 -- order_details
 CREATE TABLE public.order_details (
+    PRIMARY KEY (order_id, product_id),
     unit_price NUMERIC(9,2),
     quantity INTEGER,
     discount NUMERIC(3,2),
     order_id INTEGER,
     product_id INTEGER,
-    PRIMARY KEY (order_id, product_id),
     CONSTRAINT fk_order_details_order_id
         FOREIGN KEY (order_id) REFERENCES public.orders (order_id),
     CONSTRAINT fk_order_details_product_id
